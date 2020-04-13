6ea45a96380e42b8944a87663632dfb9dc8c969f
Metanka <themetanka@gmail.com>
2020-04-13 07:30:53 +0300

    bugfix/js: исправить недочеты в js

--- script.js
+++ script.js
@@ -3,29 +3,27 @@ const editButton = document.querySelector('.edit-button');
 const addButton = document.querySelector('.add-button');
 const popupForm = popup.querySelector('.popup__container');
 const popupClose = popup.querySelector('.popup__close');
-
-
-editButton.addEventListener('click', function () {
-  popup.classList.toggle('popup_hidden');
-});
-
-popupClose.addEventListener('click', function() {
-  popup.classList.toggle('popup_hidden');
-});
+const nameInput = document.querySelector('.popup__input_type-name');
+const jobInput = document.querySelector('.popup__input_type-job');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    let nameInput = document.querySelector('.popup__input_type-name');
-    let jobInput = document.querySelector('.popup__input_type-job');
-
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-    
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value; 
-    popup.classList.toggle('popup_hidden');
-
+    changeClassPopupHidden();
 }
 
+function changeClassPopupHidden() {
+  popup.classList.toggle('popup_hidden')
+}
+
+popupClose.addEventListener('click', changeClassPopupHidden);
 popupForm.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', function () {
+  changeClassPopupHidden();
+  nameInput.value = document.querySelector('.profile__title').textContent;
+  jobInput.value = document.querySelector('.profile__subtitle').textContent;
+});
