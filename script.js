b5b0f103397f88071d30682a3709d8e991d5e6af
Metanka <themetanka@gmail.com>
2020-04-24 18:24:27 +0300

    add добавить карточки из массива

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const nameInput = document.querySelector('.popup__input_type-name');
 const jobInput = document.querySelector('.popup__input_type-job');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
+const elements = document.querySelector('.elements');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
@@ -27,3 +28,53 @@ editButton.addEventListener('click', function () {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 });
+
+
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+/*initialCards.forEach(function(item, i, initialCards) {
+  addCard();
+});*/
+
+
+const addCard = function(i) {
+    const elementTemplate = document.querySelector('#card').content;
+    const elementCard = elementTemplate.cloneNode(true);
+    const elementTitle = elementCard.querySelector('.element__title');
+    const elementImage = elementCard.querySelector('.element__image');  
+    elementTitle.textContent = initialCards[i].name;
+      elementImage.src = initialCards[i].link;
+      elementImage.setAttribute('alt', `Изображение ${i}`); 
+    
+    console.log({elementCard})
+    elements.append(elementCard);
+};
+for (let i = 0; i < initialCards.length; i++) {
+  addCard(i);
+};
