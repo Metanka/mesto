46083a277d04844cdb5db501cf42c178658b117d
Metanka <themetanka@gmail.com>
2020-05-22 15:37:33 +0300

    bugfix-2

--- validate.js
+++ validate.js
@@ -19,53 +19,49 @@ const behaviorInputError = (formElement, inputElement, formObject, errorMessage 
 };
 //переключает стили в зависимости от валидности
 const checkInputValidity = (formElement, inputElement, formObject) => {
-  if (!inputElement.validity.valid) {
-    behaviorInputError(formElement, inputElement, formObject, inputElement.validationMessage);
-  } else {
-    behaviorInputError(formElement, inputElement, formObject);
-  }
+  behaviorInputError(formElement, inputElement, formObject, inputElement.validationMessage);
 };
 //возвращает если хотябы один не валиден
-const hasInvalidInput = inputList => {
-  return inputList.some(input => {
+const hasInvalidInput = inputElements => {
+  return inputElements.some(input => {
     return !input.validity.valid;
   });
 };
 //переключает кнопку в зависимости от валидности
-const toggleButtonState = (inputList, buttonElement, formObject) => {
-  const hasInvalid = hasInvalidInput(inputList);
+const toggleButtonState = (inputElements, buttonElement, formObject) => {
+  const hasInvalid = hasInvalidInput(inputElements);
   buttonElement.classList.toggle(formObject.inactiveButtonClass, hasInvalid);
   buttonElement.disabled = hasInvalid;
 };
 //ко всем инпутам/кнопкам добавляет слушателей на валидность
 const setEventListeners = (formElement, formObject) => {
-  const inputList = Array.from(formElement.querySelectorAll(formObject.inputSelector));
+  const inputElements = Array.from(formElement.querySelectorAll(formObject.inputSelector));
   const buttonElement = formElement.querySelector(formObject.submitButtonSelector);
-  toggleButtonState(inputList, buttonElement, formObject);
-  inputList.forEach((inputElement) => {
+  toggleButtonState(inputElements, buttonElement, formObject);
+  inputElements.forEach((inputElement) => {
     inputElement.setValue = function (value) {
       inputElement.value = value;
-      toggleButtonState(inputList, buttonElement, formObject);
+      toggleButtonState(inputElements, buttonElement, formObject);
       checkInputValidity(formElement, inputElement, formObject);
     };
 
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement, formObject);
-      toggleButtonState(inputList, buttonElement, formObject);
+      toggleButtonState(inputElements, buttonElement, formObject);
     });
   });
 };
 
 const clearForm = (formElement, formObject) => {
-  const inputsList = Array.from(formElement.querySelectorAll(formObject.inputSelector));
-  inputsList.forEach(input => {
+  const inputElements = Array.from(formElement.querySelectorAll(formObject.inputSelector));
+  inputElements.forEach(input => {
     input.setValue('');
   });
 };
 //валидация на все формы, инпуты и кнопки
 const enableValidation = (formObject) => {
-  const formsList = Array.from(document.querySelectorAll(formObject.formSelector));
-  formsList.forEach((formElement) => {
+  const formElements = Array.from(document.querySelectorAll(formObject.formSelector));
+  formElements.forEach((formElement) => {
     setEventListeners(formElement, formObject);
     formElement.addEventListener('submit', function (evt) {
       evt.preventDefault();
