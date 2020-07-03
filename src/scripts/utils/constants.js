a53b45d620b1960c7f5ebfac791087b331cbd263
Metanka <61090329+Metanka@users.noreply.github.com>
2020-07-03 15:30:40 +0300

    final (#13)

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -4,6 +4,7 @@ export const editButton = document.querySelector('.edit-button');
 export const addButton = document.querySelector('.add-button');
 export const popupFormProfile = document.querySelector('.popup__container_profile');
 export const popupFormImg = document.querySelector('.popup__container_img');
+export const popupFormAvatar = document.querySelector('.popup__container_avatar');
 export const avatar = document.querySelector('.profile__avatar');
 export const counter = '.element__counter';
 export const cardListSelector = '#card';
@@ -11,11 +12,13 @@ export const containerSelector = '.elements';
 export const popupProfileSelector = 'popup_profile';
 export const popupDeleteSelector = 'popup_delete';
 export const popupAddImgSelector = 'popup_add-img';
+export const popupAvatarSelector = 'popup_change-avatar';
 export const popupViewSelector = 'popup_view';
 export const profileTitle = 'profile__title';
 export const profileSubtitle = 'profile__subtitle';
 export const token = 'e346800a-2f73-4d6f-bc1a-ba9dbf84295f';
 export const cards = document.querySelectorAll('.element');
+export const editAvatar = document.querySelector('.profile__edit');
 export const formObject = {
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__btn',
